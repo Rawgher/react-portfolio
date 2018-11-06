55fef6f8ac64f8585ca4a5a30c6cb28f82efb72d
Roger <rdpender3@gmail.com>
2018-11-05 22:14:13 -0800

    basic contact and about pages

--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -1,10 +1,89 @@
 import React from "react";
 import Nav from "../../components/Nav";
+import Typography from "@material-ui/core/Typography";
+import { Col, Row, Container } from "../../components/Grid";
+import "./About.css";
 
 const About = () => (
   <div>
     <Nav />
-    <div>About Page</div>
+    <Container fluid>
+      <Row>
+        <Col size="md-4">
+          <div className="center-align">
+            <img
+              src="./images/RogerPender.jpg"
+              alt="Roger Pender"
+              className="myImg"
+            />
+          </div>
+        </Col>
+        <Col size="md-8">
+          <Row>
+            <Typography variant="h5" gutterBottom>
+              I graduated from UC Davis in 2014 with a Bachelor's Degree in
+              Animal Biology. I'm currently located in Northern California, but
+              I spent the majority of my life down in Los Angeles.
+            </Typography>
+            <Typography variant="h5" gutterBottom>
+              I'm a nerdy kid who enjoys comics, spending a lot of time on
+              Netflix and browsing Reddit. I spend too much time bothering and
+              taking pictures of my pets (Mabel, Shadow & Lola).
+            </Typography>
+          </Row>
+          <Row>
+            <Col size="md-6">
+              <img src="./images/mabel.jpg" alt="Mabel" className="petImg" />
+            </Col>
+            <Col size="md-6">
+              <img
+                src="./images/shadow-lola.jpg"
+                alt="Shadow and Lola"
+                className="petImg"
+              />
+            </Col>
+          </Row>
+        </Col>
+      </Row>
+      <Row>
+        <Typography variant="h5" gutterBottom>
+          I've worked with different species of animals during all sorts of
+          jobs, and had always thought I would keep working with them, until I
+          discovered coding. I really love building stuff from scratch and am
+          looking forward to seeing all the different ways I can do that. Please
+          contact me if there are any projects I can help you with.
+        </Typography>
+      </Row>
+      <Row>
+        <Typography variant="h5" gutterBottom>
+          Technologies Known
+        </Typography>
+      </Row>
+      <Typography variant="h6" gutterBottom>
+        <Row>
+          <Col size="md-2">
+            <ul>
+              <li>HTML5</li>
+              <li>CSS3</li>
+              <li>Javascript</li>
+              <li>JQuery</li>
+              <li>Bootstrap</li>
+              <li>Firebase</li>
+            </ul>
+          </Col>
+          <Col size="md-2">
+            <ul>
+              <li>Node Js</li>
+              <li>MySQL</li>
+              <li>MongoDB</li>
+              <li>Express</li>
+              <li>Handlebars JS</li>
+              <li>React Js</li>
+            </ul>
+          </Col>
+        </Row>
+      </Typography>
+    </Container>
   </div>
 );
 
--- src/pages/Contact/Contact.js
+++ src/pages/Contact/Contact.js
@@ -1,10 +1,51 @@
 import React from "react";
 import Nav from "../../components/Nav";
+import { Row, Container } from "../../components/Grid";
+import Typography from "@material-ui/core/Typography";
+import "./Contact.css";
 
 const Contact = () => (
   <div>
     <Nav />
-    <div>Contact Page</div>
+    <Container fluid>
+      <Typography variant="h5" gutterBottom>
+        Reach out to me if you have some time!
+      </Typography>
+      <Row>
+        <img
+          src="./images/github-logo.svg"
+          alt="Github Logo"
+          className="logoSize marRight"
+        />
+        {/* thank you Dave Gandy from flaticon.com for the logo */}
+        <a
+          href="https://github.com/Rawgher"
+          target="_blank"
+          rel="noreferrer noopener"
+        >
+          Github
+        </a>
+      </Row>
+      <Row>
+        <img
+          src="./images/linkedin-logo.svg"
+          alt="LinkedIn Logo"
+          className="logoSize marRight"
+        />
+        {/* thank you SimpleIcon from flaticon.com for the logo */}
+        <a
+          href="https://www.linkedin.com/in/roger-pender-2b564bb4"
+          target="_blank"
+          rel="noreferrer noopener"
+        >
+          LinkedIn
+        </a>
+      </Row>
+      <Row>
+        <i className="material-icons marRight">mail</i>
+        rdpender3@gmail.com
+      </Row>
+    </Container>
   </div>
 );
 
