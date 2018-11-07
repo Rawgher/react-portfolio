fd327d06a556fbdd618475ea4eae1218fde86bf1
Roger <rdpender3@gmail.com>
2018-11-06 23:06:06 -0800

    working on about page

--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -9,50 +9,53 @@ const About = () => (
     <Nav />
     <Container fluid>
       <Row>
-        <Col size="md-4">
-          <div className="center-align">
-            <img
-              src="./images/RogerPender.jpg"
-              alt="Roger Pender"
-              className="myImg"
-            />
-          </div>
-        </Col>
         <Col size="md-8">
           <Row>
-            <Typography variant="h5" gutterBottom>
+            <Typography
+              variant="h5"
+              gutterBottom
+              className="flow-text line-height"
+            >
               I graduated from UC Davis in 2014 with a Bachelor's Degree in
               Animal Biology. I'm currently located in Northern California, but
               I spent the majority of my life down in Los Angeles.
             </Typography>
-            <Typography variant="h5" gutterBottom>
+            <Typography
+              variant="h5"
+              gutterBottom
+              className="flow-text line-height"
+            >
               I'm a nerdy kid who enjoys comics, spending a lot of time on
               Netflix and browsing Reddit. I spend too much time bothering and
               taking pictures of my pets (Mabel, Shadow & Lola).
             </Typography>
+            <Typography
+              variant="h5"
+              gutterBottom
+              className="flow-text line-height"
+            >
+              I've worked with different species of animals during all sorts of
+              jobs, and had always thought I would keep working with them, until
+              I discovered coding. I really love building stuff from scratch and
+              am looking forward to seeing all the different ways I can do that.
+              Please contact me if there are any projects I can help you with.
+            </Typography>
           </Row>
-          <Row>
-            <Col size="md-6">
+        </Col>
+        <Col size="md-4">
+          <div className="center-align">
+            <Row>
               <img src="./images/mabel.jpg" alt="Mabel" className="petImg" />
-            </Col>
-            <Col size="md-6">
+            </Row>
+            <Row>
               <img
                 src="./images/shadow-lola.jpg"
                 alt="Shadow and Lola"
                 className="petImg"
               />
-            </Col>
-          </Row>
+            </Row>
+          </div>
         </Col>
-      </Row>
-      <Row>
-        <Typography variant="h5" gutterBottom>
-          I've worked with different species of animals during all sorts of
-          jobs, and had always thought I would keep working with them, until I
-          discovered coding. I really love building stuff from scratch and am
-          looking forward to seeing all the different ways I can do that. Please
-          contact me if there are any projects I can help you with.
-        </Typography>
       </Row>
       <Row>
         <Typography variant="h5" gutterBottom>
