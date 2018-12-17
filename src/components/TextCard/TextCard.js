8183b0af204e022038c1c241c9984a3549c02f45
Roger <rdpender3@gmail.com>
2018-12-16 16:58:29 -0800

    fixed ipad pro for portfolio

--- src/components/TextCard/TextCard.js
+++ src/components/TextCard/TextCard.js
@@ -1,6 +1,5 @@
 import React from "react";
 import PropTypes from "prop-types";
-import { withStyles } from "@material-ui/core/styles";
 import Card from "@material-ui/core/Card";
 import CardActions from "@material-ui/core/CardActions";
 import CardContent from "@material-ui/core/CardContent";
@@ -8,29 +7,17 @@ import Button from "@material-ui/core/Button";
 import Typography from "@material-ui/core/Typography";
 import "./TextCard.css";
 
-const styles = {
-  title: {
-    fontSize: 42
-  },
-  pos: {
-    // marginBottom: 30,
-    fontSize: 22
-  }
-};
-
 function SimpleCard(props) {
-  const { classes } = props;
-
   return (
     <Card className="cardBody divHeight">
       <CardContent className="textPositioning">
-        <Typography className={classes.title} color="textPrimary" gutterBottom>
+        <Typography className="textTitle" color="textPrimary" gutterBottom>
           {props.name}
         </Typography>
-        <Typography className={classes.pos} color="textPrimary">
+        <Typography className="textPos" color="textPrimary">
           {props.description}
         </Typography>
-        <Typography className={classes.pos} color="textPrimary">
+        <Typography className="textPos" color="textPrimary">
           <em>{props.login}</em>
         </Typography>
       </CardContent>
@@ -62,4 +49,4 @@ SimpleCard.propTypes = {
   classes: PropTypes.object.isRequired
 };
 
-export default withStyles(styles)(SimpleCard);
+export default SimpleCard;
